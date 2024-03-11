f33f7c8ba0d93e351df19cfb94ebfad2e87e7b83
Om Priya <ompriya18153789@gmail.com>
2024-03-11 14:46:39 +0530

    Project setup done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipe.model"
 
 @Component({
   selector: 'shop-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe("Test 1", "Test description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_aukbAs01Ium9M82SkFcUJGDErmEgdwAn0QprMn_wWQ&s"),
+    new Recipe("Test 1", "Test description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_aukbAs01Ium9M82SkFcUJGDErmEgdwAn0QprMn_wWQ&s"),
+    new Recipe("Test 1", "Test description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_aukbAs01Ium9M82SkFcUJGDErmEgdwAn0QprMn_wWQ&s"),
+  ];
 }
