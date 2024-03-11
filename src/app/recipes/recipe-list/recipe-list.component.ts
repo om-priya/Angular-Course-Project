7b4c7203f6887d6190d93171075cd403bcda66b4
Om Priya <ompriya18153789@gmail.com>
2024-03-11 10:20:44 +0530

    Basic Setup Done

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shop-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+
+}
